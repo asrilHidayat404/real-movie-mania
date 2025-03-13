b7bff95d7e6c1d1ea2a06cbe18fc6b6114e8006d
asrilHidayat404 <asril.code404@gmail.com>
2025-03-13 23:14:13 +0700

    welcome to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,20 @@ const page = async () => {
     redirect("/");
   }
 
-  return <div>page</div>;
+  return (
+    <main className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold mb-5">Selamat Datang di welcome</h1>
+      <div>
+        <img
+          src={user.image ? user.image : "https://via.placeholder.com/300"}
+          alt="..."
+          width={300}
+          className="rounded-full mb-4"
+        />
+        <p className="text-center font-bold text-2xl">{user.name}</p>
+      </div>
+    </main>
+  );
 };
 
 export default page;
